2d089a20299aee9335fa4c1e3d90494c1064677f
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:26:26 -0300

    🎯 Testes com Cypress

---
+++ cypress/integration/hardcoded-assertion/sample.spec.js
@@ -0,0 +1,28 @@
+describe('Hardcoded assertion bad practice', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('getStories')
+
+    cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
+  })
+
+  it('searches', () => {
+    const { hits } = require('../../fixtures/stories')
+
+    cy.search('cypress.io')
+    cy.wait('@getStories')
+
+    cy.get('.table-row')
+      .as('tableRows')
+      .should('have.length', hits.length)
+    hits.forEach((hit, index) => {
+      cy.get('@tableRows')
+        .eq(index)
+        .should('contain', hit.title)
+      })  
+    })
+  })
