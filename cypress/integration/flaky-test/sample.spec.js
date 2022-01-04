85e1349cb5f0bc944f48eb341bd792d8711a40d2
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:23:36 -0300

    🎯 Testes com Cypress

---
+++ cypress/integration/flaky-test/sample.spec.js
@@ -0,0 +1,27 @@
+describe('Flaky tests bad practice', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('getStories')
+
+    cy.visit('https://wlsf82-hacker-stories.web.app')
+    //cy.wait('@getStories')
+  })
+
+  Cypress._.times(10, () => {
+    it('shows a max of 5 buttons for the last searched terms', () => {
+      const faker = require('faker')
+
+      Cypress._.times(6, () => {
+        cy.search(faker.random.word())
+      })
+
+      //cy.wait('@getStories')
+
+      cy.get('.last-searches button')
+        .should('have.length', 5)
+    })
+  })
+})
