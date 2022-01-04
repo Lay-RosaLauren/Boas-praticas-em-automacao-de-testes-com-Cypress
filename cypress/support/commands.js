b57d910020c9259481de03bdb6c28140347c82b8
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:56:31 -0300

    🎯 Testes com Cypress

---
+++ cypress/support/commands.js
@@ -0,0 +1,23 @@
+Cypress.Commands.add('search', term => {
+  cy.get('input[type="text"]')
+    .should('be.visible')
+    .clear()
+    .type(`${term}{enter}`)
+})
+
+//Cypress.Commands.add('assertResults', () => {
+  //cy.get('.table-row').then(rows => {
+    //expect(rows.length).to.be.at.least(1)
+  //})
+//})
+
+Cypress.Commands.add('updateDestination', data => {
+  cy.get('#destination_name')
+    .clear()
+    .type(data.name)
+  cy.get('#destination_description')
+    .clear()
+    .type(data.description)
+  cy.get('input[type="submit"]')
+    .click()   
+})
