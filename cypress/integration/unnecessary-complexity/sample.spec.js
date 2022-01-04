158be738dd1e0d03cc51dc7355bd4d5cca36e3ad
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:42:33 -0300

    🎯 Testes com Cypress

---
+++ cypress/integration/unnecessary-complexity/sample.spec.js
@@ -0,0 +1,18 @@
+describe('Unnecessary complexity anti-patter', () => {
+  beforeEach(() => {
+    cy.visit('https://bit.ly/2XSuwCW')
+
+    if (Math.random() > 0.5) {
+      cy.get('#agree')
+        .click() 
+    }
+  })
+
+  Cypress._.times(5, () => {
+    it('checks the checkbox only if not checked', () => {
+      cy.get('#agree')
+        .check()
+        .should('be.checked')
+    })
+  })
+})
