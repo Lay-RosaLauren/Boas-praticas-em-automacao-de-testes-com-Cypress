cebba6ae051d2dcb813be77bddfc86883d37120e
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:34:12 -0300

    🎯 Testes com Cypress

---
+++ cypress/integration/page-object/sample.spec.js
@@ -0,0 +1,28 @@
+const faker = require('faker')
+
+describe('Page Object bad practice', () => {
+  const randomDestination = Math.floor(Math.random() * 15) + 1
+
+  beforeEach(() => {
+    cy.visit(`https://lit-chamber-61567.herokuapp.com/destinations/${randomDestination}/edit`)
+  })
+
+  it('updates destination info', () => {
+    const info = {
+      name: faker.random.words(5),
+      description: faker.random.words(5)
+    }
+
+    cy.updateDestination(info)
+
+    cy.url()
+      .should(
+        'be.equal',
+        `https://lit-chamber-61567.herokuapp.com/destinations/${randomDestination}`
+      )
+    cy.contains('h2', info.name)
+      .should('be.visible')
+    cy.contains('p', info.description)
+      .should('be.visible')
+  })
+})
