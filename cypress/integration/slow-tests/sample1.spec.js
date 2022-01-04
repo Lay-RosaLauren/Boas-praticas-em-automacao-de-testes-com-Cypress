2e5784c3524cf309fed54e05e2bf92f68b8525ae
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:39:44 -0300

    🎯 Testes com Cypress

---
+++ cypress/integration/slow-tests/sample1.spec.js
@@ -0,0 +1,29 @@
+describe('Slow tests bad practice - Sample 1', () => {
+  beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('getStories')
+
+    cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
+
+    cy.get('input[type="text"]')
+      .should('be.visible')
+      .and('have.value', 'redux')
+      .as('searchField')
+      .clear()
+  })
+
+  it('searches by typing and hitting enter', () => {
+    const { hits } = require('../../fixtures/stories')
+    cy.get('@searchField')
+      .type('frontend testing{enter}')
+
+    cy.wait('@getStories')
+
+    cy.get('.table-row')
+      .should('have.length', hits.length)
+  })
+})
