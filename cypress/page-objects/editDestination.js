e91ea70032e7ccfc7eaace41ccf129c5c07d400f
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:51:43 -0300

    🎯 Testes com Cypress

---
+++ cypress/page-objects/editDestination.js
@@ -0,0 +1,16 @@
+//const editDestination = {
+  //updateInfo: data => {
+    //cy.get('#destination_name')
+      //.clear()
+      //.type(data.name)
+    //cy.get('#destination_description')
+      //.clear()
+      //.type(data.description)
+    //cy.get('input[type="submit"]')
+      //.click()
+  //}
+//}
+
+//module.exports = editDestination
+
+//Como não estamos mais usando pode ser deletada essa pasta deste arquivo.
