8ba4fe10ac013fd95c2a5391effb99689cdd677f
Rosa Lauren Barreira Nuevo de Campos <charmehaut@uol.com.br>
2022-01-04 17:16:13 -0300

    🎯 Testes com Cypress

---
+++ cypress/integration/browser-testing/sample1.spec.js
@@ -0,0 +1,11 @@
+describe('Browser testing bad practice - Sample 1', () => {
+  beforeEach(() => {
+    cy.visit('https://notes-serverless-app.com')
+  })
+
+  it('directs the user to the login page when clicking the login link', () => {
+    cy.contains('.nav a', 'Login')
+      .should('have.attr', 'href', '/login')
+      .and('not.have.attr', 'target')
+  })
+})
